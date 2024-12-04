2e911c5283b5729f7abe1409790aebc52ab3130d
joaohnt <jpedrooo.cg@gmail.com>
2024-12-04 00:06:21 -0300

    project created

---
+++ src/test/kotlin/com/joaozada/mercadolivro2/Mercadolivro2ApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.joaozada.mercadolivro2
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class Mercadolivro2ApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
